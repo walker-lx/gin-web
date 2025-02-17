f4c3dca8d1bf02880c900f43531f6b1482075cf7
alen <1217336430@qq.com>
2025-02-17 22:58:26 +0800

    feat: ascii

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+
+	// get
 	r.GET("/gin", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello",
 		})
+	})
+
+	// AsciiJSON
+	r.GET("someJson", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+		c.AsciiJSON(200, data)
 	})
 	r.Run(":8000")
 }
